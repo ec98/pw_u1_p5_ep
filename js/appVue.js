b202a507cbca40c4b700d6a52f5ed0b7f42081b0
ec98 <carlospiruch98@gmail.com>
2023-12-13 19:46:35 -0500

    directiva v-for, v-model

--- js/appVue.js
+++ js/appVue.js
@@ -1,11 +1,24 @@
 console.log("elementos Vue");
 
-// mostrador
+// funciones de Vue
 console.log(Vue);
 
+// arreglo de estudiantes
+const estudiantes = [
+    {nombre: "Edwin", apellido:"Piruch"},
+    {nombre: "Maria", apellido:"Angela"},
+    {nombre: "Sofia", apellido:"Martha"},
+    {nombre: "Lucy", apellido:"Lopez"},
+    {nombre: "Naomi", apellido:"Moya"}
+]
+console.log(estudiantes)
+console.table(estudiantes)
+// propiedades reactivas -> terminan funcionando como un modelo (vista-controlador)
+
+
 const app = Vue.createApp({
+
     /*template : `
-    
     <h1>hola mundo</h1>
     <p> Desde App.js</p>
     <h6>{{1+1}}</h6>
@@ -24,15 +37,41 @@ const app = Vue.createApp({
         console.log("cambiar numero");
         console.log(this.valor);
         this.valor++;
+    },
+    agregarEstudiante(){
+        console.log("agregando estudiante");
+        const estu = {nombre: this.nombre, apellido: this.apellido}; //crear el objeto de tipo estudiante
+        // this.lista.unshift(estu); //agregar elemento al inicio
+        this.lista.push(estu); //agregar elemento al final
+        // this.lista.pop(estu) //eliminar elemento en la lista
+    },
+    // otra propiedad
+    presionandoTecla(event){
+        console.log("presionando....");
+        console.log(event);
+        // atributo charcode
+        console.log(event.charCode);
+    },
+    presionarTelca(event){
+        console.log("enter")
+        const elem = 13;
+        if(event === elem){
+            agregarEstudiante();
+        }
     }
    },
    watch:{
 
    },
+//    propiedad reactiva
    data(){
     return {
         mensaje: 'cambiado el mensaje desde un this.mensaje',
-        valor: 10
+        valor: 10,
+        // no podemos renderizar el vector.
+        lista:estudiantes,
+        nombre:null,
+        apellido:null
     }
    }
 });
